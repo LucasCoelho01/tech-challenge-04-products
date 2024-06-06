cf3ea308c21ca3fceaa024a766f828c8e4c968f2
Lucas Coelho dos Santos <lucas.goleiro1995@gmail.com>
2024-06-06 18:27:25 -0300

    first commit

---
+++ src/main/java/com/tech_challenge_04/products/TechChallenge04ProductsApplication.java
@@ -0,0 +1,13 @@
+package com.tech_challenge_04.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechChallenge04ProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechChallenge04ProductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tech_challenge_04/products/TechChallenge04ProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech_challenge_04.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechChallenge04ProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
