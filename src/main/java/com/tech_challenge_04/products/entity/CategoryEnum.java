e4e0d0386610424b802890408939f662c5c4d622
Lucas Coelho dos Santos <lucas.goleiro1995@gmail.com>
2024-06-06 19:13:35 -0300

    Created base folders and files

---
+++ src/main/java/com/tech_challenge_04/products/entity/CategoryEnum.java
@@ -0,0 +1,8 @@
+package com.tech_challenge_04.products.entity;
+
+public enum CategoryEnum {
+    LANCHE,
+    ACOMPANHAMENTO,
+    BEBIDA,
+    SOBREMESA;
+}
---
+++ src/main/java/com/tech_challenge_04/products/entity/Product.java
@@ -0,0 +1,4 @@
+package com.tech_challenge_04.products.entity;
+
+public class Product {
+}
